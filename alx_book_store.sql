5c2ecc1f98fe8b7c43980413c25ade6208c50221
KHALID ELFARAJ <162966628+khalidFRJ@users.noreply.github.com>
2024-07-26 10:11:17 +0100

    Update alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -23,7 +23,7 @@ CREATE TABLE Customers (
 
 CREATE TABLE Orders (
  order_id INT Primary Key,
- customer_id INT,
+ customer_id INT FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
  order_date DATE
 );
 

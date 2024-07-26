fcb95d15b0f7b5e2845179f331ec367305648d8c
KHALID ELFARAJ <162966628+khalidFRJ@users.noreply.github.com>
2024-07-26 11:20:12 +0100

    Update MySQLServer.py

--- MySQLServer.py
+++ MySQLServer.py
@@ -2,4 +2,6 @@ import mysql.connector
 conn = mysql.connector.connect(host='localhost' , password = 'khalid' , user = 'root')
 if conn.is_connected():
    print("connection etablished")
+   
+CREATE DATABASE IF NOT EXISTS alx_book_store
 

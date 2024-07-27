75805330b97553fef1edea4e4bfb0dc0c074c6ab
KHALID ELFARAJ <162966628+khalidFRJ@users.noreply.github.com>
2024-07-27 11:12:48 +0100

    Update task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -1,3 +1,3 @@
-INSERT INTO customers VALUES (1, "Cole Baidoo","cbaidoo@sandtech.com","123 Happiness Ave.");
+INSERT INTO customer VALUES (1, "Cole Baidoo","cbaidoo@sandtech.com","123 Happiness Ave.");
 
 
